9f3824406705e8c7f9b1e80c35bfb1c9515e77ea
Игорь Прохоров <iprokhorov@mobicont.ru>
2018-06-28 22:56:44 +0300

    first steps

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,23 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML // fx:id="button1"
+    private Button button1;
+
+    @FXML
+    void onAction(ActionEvent event) {
+        System.out.println("Pressed!");
+    }
+
 }
+
+/*
+<GridPane fx:controller="sample.Controller"
+          xmlns:fx="http://javafx.com/fxml" alignment="center" hgap="10" vgap="10">
+</GridPane>
+ */
